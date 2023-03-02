d32d6c93cf204d8c9078e65afac5f4437b050aa1
Samuel <samuelpl0888@gmail.com>
2023-03-02 00:53:11 -0400

    Setup auth controllers and routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,29 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import 'express-async-errors';
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 5000;
 import prisma from './types/prisma';
+// Routes
+import authRouter from './routes/authRoutes';
 // Extra packages
 import morgan from 'morgan';
+// Middleware
+import notFound from './middleware/notFound';
+import errorHandlerMiddleware from './middleware/error-handler';
 
 app.use(express.json());
 app.use(morgan('tiny'));
 
+app.use('/api/auth', authRouter);
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello world');
 });
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 // Start app when connection to db successful
 const start = async () => {
---
+++ src/controllers/authControllers.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnauthenticatedError } from '../errors';
+import prisma from '../services/prisma';
+
+export const register = async (req: Request, res: Response) => {
+  const { name, lastName, email, password } = req.body;
+
+  // ! Check if all fields are filled
+  if (!name || !lastName || !email || !password) {
+    throw new BadRequestError('Provide all fields, try again');
+  }
+
+  // ! Check if email already exists
+  const emailAlreadyExists = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (emailAlreadyExists) {
+    throw new BadRequestError('Email already exists, please provide other value');
+  }
+
+  // * Create user
+  const user = await prisma.user.create({
+    data: {
+      name,
+      lastName,
+      email,
+      password,
+    },
+  });
+
+  res.status(StatusCodes.CREATED).json({ status: 'success', user });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // ! Check if all fields are filled
+  if (!email || !password) {
+    throw new BadRequestError('Provide all fields, try again');
+  }
+
+  // ! Check if user exists
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  // * Login user
+  res.status(StatusCodes.OK).json({ status: 'success', user });
+};
