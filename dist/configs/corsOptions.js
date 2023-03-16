7ac29dce19fb29921eb5c053737525ced13d8460
Samuel <samuelpl0888@gmail.com>
2023-03-15 22:09:16 -0400

    Update CORS Options

--- dist/configs/corsOptions.js
+++ dist/configs/corsOptions.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5000', 'https://auth-w-react-zod-ts.vercel.app'],
+    origin: ['https://auth-w-react-zod-ts.vercel.app', 'http://localhost:5173', 'http://localhost:5000'],
     credentials: true,
 };
 exports.default = corsOptions;
--- src/configs/corsOptions.ts
+++ src/configs/corsOptions.ts
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5000', 'https://auth-w-react-zod-ts.vercel.app'],
+  origin: ['https://auth-w-react-zod-ts.vercel.app', 'http://localhost:5173', 'http://localhost:5000'],
   credentials: true,
 };
 export default corsOptions;
